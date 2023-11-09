bad28ddfff85232ae9b17f7adc7a91a7e95b9af1
Rogelio <149198850+rogeermv@users.noreply.github.com>
2023-11-09 00:54:18 -0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -18,7 +18,7 @@ async def test_7seg(dut):
     dut.rst_n.value = 1
 
     dut._log.info("check all segments")
-    for i in range(8):
+    for i in range(10):
         dut._log.info("check segment {}".format(i))
         await ClockCycles(dut.clk, 1000)
         assert int(dut.segments.value) == segments[i]
