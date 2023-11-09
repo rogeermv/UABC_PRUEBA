7b37b3bd4a892e90f086eb5b53e7598ed7ed47e6
Rogelio <149198850+rogeermv@users.noreply.github.com>
2023-11-09 00:52:01 -0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -21,6 +21,7 @@ async def test_7seg(dut):
     for i in range(8):
         dut._log.info("check segment {}".format(i))
         await ClockCycles(dut.clk, 1000)
+        assert int(dut.segments.value) == segments[i]
 
         # all bidirectionals are set to output
         assert dut.uio_oe == 0xFF
