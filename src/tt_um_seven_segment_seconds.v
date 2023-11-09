6296090904e184fa37b4c7fc35389b2de8258e0d
Rogelio <149198850+rogeermv@users.noreply.github.com>
2023-11-08 19:11:48 -0800

    Update tt_um_seven_segment_seconds.v

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -26,35 +26,27 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
     reg [23:0] second_counter;
     reg [3:0] digit;
 
-    // if external inputs are set then use that as compare count
-    // otherwise use the hard coded MAX_COUNT
-    wire [23:0] compare = ui_in == 0 ? MAX_COUNT: {6'b0, ui_in[7:0], 10'b0};
-
-    always @(posedge clk) begin
+    aalways @(posedge clk) begin
         // if reset, set counter to 0
         if (reset) begin
             second_counter <= 0;
             digit <= 0;
         end else begin
             // if up to 16e6
-            if (second_counter == compare) begin
+            if (second_counter == MAX_COUNT) begin
                 // reset
                 second_counter <= 0;
 
                 // increment digit
                 digit <= digit + 1'b1;
 
-                // only count from 0 to 9
-                if (digit == 9)
+                if (digit == 8)
                     digit <= 0;
-
             end else
                 // increment counter
                 second_counter <= second_counter + 1'b1;
         end
     end
-
     // instantiate segment display
     seg7 seg7(.counter(digit), .segments(led_out));
-
 endmodule
