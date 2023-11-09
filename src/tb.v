4717240c273edfed5fd25394ae75fcf2a16b0125
Rogelio <149198850+rogeermv@users.noreply.github.com>
2023-11-08 21:24:56 -0800

    Update tb.v

--- src/tb.v
+++ src/tb.v
@@ -28,7 +28,7 @@ module tb ();
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    tt_um_seven_segment_seconds tt_um_seven_segment_seconds (
+    tt_um_electronica_uabc_mexicali_2023 tt_um_electronica_uabc_mexicali_2023 (
     // include power ports for the Gate Level test
     `ifdef GL_TEST
         .VPWR( 1'b1),
